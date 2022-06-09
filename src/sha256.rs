82bc607076929c71db7f683290d00e0d710e6e27
Michael Stewart <mikrostew@gmail.com>
2022-06-08 23:30:49 -0400

    copy rest of spec info, fix things, and clean up comments

--- src/sha256.rs
+++ src/sha256.rs
@@ -2,13 +2,19 @@
 // https://csrc.nist.gov/publications/detail/fips/180/4/final
 // (pdf: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf)
 
-// block size is 512 bits (64 bytes, 16 32-bit words)
+// Note:
+//  comments using /* ... */ are text copied from that ^^ hash spec doc,
+//  comments using // ... are other comments
+
+/*
+   Algorithm   Message Size   Block Size   Word Size   Message Digest Size
+                  (bits)        (bits)      (bits)          (bits)
+
+   SHA-256        < 2^64         512          32             256
+*/
+
 const BLOCK_SIZE = 512 / 8;
-
-// digest size 256 bits (32 bytes)
 const DIGEST_SIZE = 256 / 8;
-
-// word size 32 bits (4 bytes)
 const WORD_SIZE = 32 / 8;
 
 // the output of the sha256 hash function
@@ -16,20 +22,100 @@ pub struct Hash {
     bytes: [u8; DIGEST_SIZE],
 }
 
-// TODO: what the symbols here mean
+/*
+   Parameters
 
-// "SHA-256 [uses] six logical functions, where each function operates on 32-bit words,
-//  which are represented as x, y, and z. The result of each function is a new 32-bit word."
-// Ch(x,y,z) = (x^y) ⊕ (-x^z)
-// Maj(x,y,z) = (x^y) ⊕ (x^z) ⊕ (y^z)
-// Σ (0 -> 256) (x) = ROTR^2 (x) + ROTR^13 (x) + ROTR^22 (x)
-// Σ (1 -> 256) (x) = ROTR^6 (x) + ROTR^11 (x) + ROTR^25 (x)
-// σ (0 -> 256) (x) = ROTR^7 (x) + ROTR^18 (x) + SHR^3 (x)
-// σ (1 -> 256) (x) = ROTR^17 (x) + ROTR^19 (x) + SHR^10 (x)
+   The following parameters are used in the secure hash algorithm specifications in this Standard.
 
-// "SHA-256 [uses a] sequence of sixty-four constant 32-bit words, ...
-//  These words represent the first thirty-two bits of the fractional parts of
-//  the cube roots of the first sixty-four prime numbers."
+    a, b, c, ..., h   Working variables that are the w-bit words used in the computation of the hash values, H(i)
+
+    H(i)              The i-th hash value. H(0) is the initial hash value; H(N) is the final hash value
+                      and is used to determine the message digest.
+
+    Hj(i)             The j-th word of the i-th hash value, where H0(i) is the left-most word of hash value i.
+
+    Kt                Constant value to be used for the iteration t of the hash computation.
+
+    k                 Number of zeroes appended to a message during the padding step.
+
+    l                 Length of the message, M, in bits.
+
+    m                 Number of bits in a message block, M(i).
+
+    M                 Message to be hashed.
+
+    M(i)              Message block i, with a size of m bits.
+
+    Mj(i)             The jth word of the ith message block, where M0(i) is the left-most word of
+                      message block i.
+
+    n                 Number of bits to be rotated or shifted when a word is operated upon.
+
+    N                 Number of blocks in the padded message.
+
+    T                 Temporary w-bit word used in the hash computation.
+
+    w                 Number of bits in a word.
+
+    Wt                The t-th w-bit word of the message schedule.
+*/
+
+/*
+   Symbols and Operations
+
+   The following symbols are used in the secure hash algorithm specifications; each operates on w-bit words.
+
+    ^          Bitwise AND operation.
+
+    ⌄          Bitwise OR (“inclusive-OR”) operation.
+
+    ⊕          Bitwise XOR (“exclusive-OR”) operation.
+
+    ¬          Bitwise complement operation.
+
+    +          Addition modulo 2^w.
+
+    <<         Left-shift operation, where x << n is obtained by discarding the left-most n
+               bits of the word x and then padding the result with n zeroes on the right.
+
+    >>         Right-shift operation, where x >> n is obtained by discarding the right-most
+               n bits of the word x and then padding the result with n zeroes on the left.
+
+   The following operations are used in the secure hash algorithm specifications:
+
+    ROTL^n(x)  The rotate left (circular left shift) operation, where x is a w-bit word and n
+               is an integer with 0 <= n < w, is defined by ROTL^n(x) = (x << n) ⌄ (x >> w - n).
+
+    ROTR^n(x)  The rotate right (circular right shift) operation, where x is a w-bit word
+               and n is an integer with 0 <= n < w, is defined by ROTR^n(x) = (x >> n) ⌄ (x << w - n).
+
+    SHR^n(x)   The right shift operation, where x is a w-bit word and n is an integer with 0 <= n < w,
+               is defined by SHR^n(x) = x >> n.
+*/
+
+/*
+   SHA-256 [uses] six logical functions, where each function operates on 32-bit words,
+   which are represented as x, y, and z. The result of each function is a new 32-bit word."
+
+    Ch(x,y,z)  = (x ^ y) ⊕ (-x ^ z)
+
+    Maj(x,y,z) = (x ^ y) ⊕ (x ^ z) ⊕ (y ^ z)
+
+    Σ0{256}(x) = ROTR^2(x) ⊕  ROTR^13(x) ⊕  ROTR^22(x)
+
+    Σ1{256}(x) = ROTR^6(x) ⊕  ROTR^11(x) ⊕  ROTR^25(x)
+
+    σ0{256}(x) = ROTR^7(x) ⊕  ROTR^18(x) ⊕  SHR^3(x)
+
+    σ1{256}(x) = ROTR^17(x) ⊕  ROTR^19(x) ⊕  SHR^10(x)
+*/
+
+/*
+   SHA-256 [uses a] sequence of sixty-four constant 32-bit words,
+   K0{256}, K1{256},..., K63{256}.
+   These words represent the first thirty-two bits of the fractional parts of
+   the cube roots of the first sixty-four prime numbers.
+*/
 const CUBE_ROOTS: [u32; 64] = [
     0x428a2f98,
     0x71374491,
@@ -97,95 +183,141 @@ const CUBE_ROOTS: [u32; 64] = [
     0xc67178f2,
 ];
 
-// Padding the Message
-//
-// "The purpose of this padding is to ensure that the padded message is a multiple of 512 or 1024
-//  bits, depending on the algorithm. Padding can be inserted before hash computation begins on a
-//  message, or at any other time during the hash computation prior to processing the block(s) that
-//  will contain the padding."
-//
-// "Suppose that the length of the message, M, is l bits. Append the bit “1” to the end of the
-//  message, followed by k zero bits, where k is the smallest, non-negative solution to the equation l+1+k = 448 mod 512.
-//  Then append the 64-bit block that is equal to the number l expressed
-//  using a binary representation. For example, the (8-bit ASCII) message “abc” has length 8 * 3 = 24,
-//  so the message is padded with a one bit, then 448-(24+1)=423 zero bits, and then
-//  the message length, to become the 512-bit padded message:
-//
-//                                 (423 bits)  (64 bits)
-//  01100001 01100010 01100011  1  00...00     00...011000
-//     "a"      "b"      "c"                       l = 24
-//
-//  The length of the padded message should now be a multiple of 512 bits."
+/*
+   Padding the Message
 
+   The purpose of this padding is to ensure that the padded message is a multiple of 512 or 1024
+   bits, depending on the algorithm. Padding can be inserted before hash computation begins on a
+   message, or at any other time during the hash computation prior to processing the block(s) that
+   will contain the padding.
 
-// Parsing the Message
-//
-// "The message and its padding must be parsed into N m-bit blocks."
-//
-// "For SHA-1, SHA-224 and SHA-256, the message and its padding are parsed into N 512-bit
-//  blocks, M(1), M(2),..., M(N). Since the 512 bits of the input block may be expressed as sixteen 32-bit
-//  words, the first 32 bits of message block i are denoted
-//  M0(i), the next 32 bits are M1(i), and so on up to M15(i).
+   Suppose that the length of the message, M, is l bits. Append the bit “1” to the end of the
+   message, followed by k zero bits, where k is the smallest, non-negative solution to the equation l+1+k = 448 mod 512.
+   Then append the 64-bit block that is equal to the number l expressed
+   using a binary representation. For example, the (8-bit ASCII) message “abc” has length 8 * 3 = 24,
+   so the message is padded with a one bit, then 448-(24+1)=423 zero bits, and then
+   the message length, to become the 512-bit padded message:
 
-// Setting the Initial Hash Value (H(0))
-//
-// "Before hash computation begins for each of the secure hash algorithms, the initial hash value,
-// H(0), must be set. The size and number of words in H(0) depends on the message digest size."
-//
-// "For SHA-256, the initial hash value, H(0), shall consist of the following eight 32-bit words, in hex:
-//  H0(0) = 6a09e667
-//  H1(0) = bb67ae85
-//  H2(0) = 3c6ef372
-//  H3(0) = a54ff53a
-//  H4(0) = 510e527f
-//  H5(0) = 9b05688c
-//  H6(0) = 1f83d9ab
-//  H7(0) = 5be0cd19
-//
-//  These words were obtained by taking the first thirty-two bits of the fractional parts of the square
-//  roots of the first eight prime numbers."
+                                   (423 bits)  (64 bits)
+    01100001 01100010 01100011  1  00...00     00...011000
+       "a"      "b"      "c"                       l = 24
 
+    The length of the padded message should now be a multiple of 512 bits.
+*/
 
-// SHA-256
-//
-// "SHA-256 may be used to hash a message, M, having a length of l bits, where 0 <= l < 2^64.
-//  The algorithm uses
-//    1) a message schedule of sixty-four 32-bit words,
-//    2) eight working variables of 32 bits each, and
-//    3) a hash value of eight 32-bit words.
-//  The final result of SHA-256 is a 256-bit message digest."
-//
-// "The words of the message schedule are labeled W0, W1,..., W63.
-//  The eight working variables are labeled a, b, c, d, e, f, g, and h.
-//  The words of the hash value are labeled H0(i), H1(i),..., H7(i),
-//  which will hold the initial hash value, H(0),
-//  replaced by each successive intermediate hash value
-//  (after each message block is processed), H(i), and ending with the final hash value, H(N).
-//  SHA-256 also uses two temporary words, T1 and T2."
+/*
+   Parsing the Message
 
-// SHA-256 Preprocessing
-//
-//  1. Set the initial hash value, H(0)
-//  2. The message is padded and parsed
+   The message and its padding must be parsed into N m-bit blocks.
 
+   For SHA-1, SHA-224 and SHA-256, the message and its padding are parsed into N 512-bit
+   blocks, M(1), M(2),..., M(N). Since the 512 bits of the input block may be expressed as sixteen 32-bit
+   words, the first 32 bits of message block i are denoted
+   M0(i), the next 32 bits are M1(i), and so on up to M15(i).
+*/
 
-// SHA-256 Hash Computation
-//
-// "The SHA-256 hash computation uses functions and constants previously defined
-//  Addition is performed modulo 2^32."
-//
-// "Each message block, M(1), M(2), ..., M(N), is processed in order, using the following steps:
-//
-//  For i=1 to N:
-//  {
-//
-//
-// <TODO>
-//
-//
-//  }
-//
-//  After repeating steps one through four a total of N times (i.e., after processing M(N)), the resulting
-//  256-bit message digest of the message, M, is:
-//
-//  H0(N) || H1(N) || H2(N) || H3(N) || H4(N) || H5(N) || H6(N) || H7(N)"
+/*
+   Setting the Initial Hash Value (H(0))
+
+   Before hash computation begins for each of the secure hash algorithms, the initial hash value,
+   H(0), must be set. The size and number of words in H(0) depends on the message digest size.
+
+   For SHA-256, the initial hash value, H(0), shall consist of the following eight 32-bit words, in hex:
+
+    H0(0) = 6a09e667
+    H1(0) = bb67ae85
+    H2(0) = 3c6ef372
+    H3(0) = a54ff53a
+    H4(0) = 510e527f
+    H5(0) = 9b05688c
+    H6(0) = 1f83d9ab
+    H7(0) = 5be0cd19
+
+   These words were obtained by taking the first thirty-two bits of the fractional parts of the square
+   roots of the first eight prime numbers.
+*/
+
+/*
+   SHA-256
+
+   SHA-256 may be used to hash a message, M, having a length of l bits, where 0 <= l < 2^64.
+   The algorithm uses
+     1) a message schedule of sixty-four 32-bit words,
+     2) eight working variables of 32 bits each, and
+     3) a hash value of eight 32-bit words.
+   The final result of SHA-256 is a 256-bit message digest.
+
+   The words of the message schedule are labeled W0, W1,..., W63.
+   The eight working variables are labeled a, b, c, d, e, f, g, and h.
+   The words of the hash value are labeled H0(i), H1(i),..., H7(i),
+   which will hold the initial hash value, H(0), replaced by each successive intermediate hash value
+   (after each message block is processed), H(i), and ending with the final hash value, H(N).
+   SHA-256 also uses two temporary words, T1 and T2.
+*/
+
+/*
+   SHA-256 Preprocessing
+
+    1. Set the initial hash value, H(0)
+    2. The message is padded and parsed
+*/
+
+/*
+   SHA-256 Hash Computation
+
+   The SHA-256 hash computation uses functions and constants previously defined
+   Addition is performed modulo 2^32.
+
+   Each message block, M(1), M(2), ..., M(N), is processed in order, using the following steps:
+
+   For i=1 to N:
+   {
+
+     1. Prepare the message schedule, {Wt}:
+
+        for 0 <= t <= 15,  Wt = Mt(i)
+
+        for 16 <= t <= 63,  Wt = σ1{256}(Wt-2) + Wt-7 + σ0{256}(Wt-15) + Wt-16
+
+     2. Initialize the eight working variables, a, b, c, d, e, f, g, and h, with the (i-1)-st hash value:
+
+        a = H0(i-1)
+        b = H1(i-1)
+        c = H2(i-1)
+        d = H3(i-1)
+        e = H4(i-1)
+        f = H5(i-1)
+        g = H6(i-1)
+        h = H7(i-1)
+
+     3. For t = 0 to 63:
+
+        T1 = h + Σ1{256}(e) + Kt{256} + Wt
+        T2 = Σ0{256}(a) + Maj(a, b, c)
+         h = g
+         g = f
+         f = e
+         e = d + T1
+         d = c
+         c = b
+         b = a
+         a = T1 + T2
+
+     4. Compute the i-th intermediate hash value H(i):
+
+        H0(i) = a + H0(i-1)
+        H1(i) = b + H1(i-1)
+        H2(i) = c + H2(i-1)
+        H3(i) = d + H3(i-1)
+        H4(i) = e + H4(i-1)
+        H5(i) = f + H5(i-1)
+        H6(i) = g + H6(i-1)
+        H7(i) = h + H7(i-1)
+
+   }
+
+   After repeating steps one through four a total of N times (i.e., after processing M(N)), the resulting
+   256-bit message digest of the message, M, is:
+
+   H0(N) || H1(N) || H2(N) || H3(N) || H4(N) || H5(N) || H6(N) || H7(N)
+*/
