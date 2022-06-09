02b7c20a680b405200096e2e0fb916100fa82ef5
Michael Stewart <mikrostew@gmail.com>
2022-06-08 22:25:15 -0400

    use an inline static str instead

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@ use objc::rc::autoreleasepool;
 use std::mem;
 
 // TODO: build this string using macros?
-static LIBRARY_SRC: &str = include_str!("compute-argument-buffer.metal");
+static LIBRARY_SRC: &str = "#include <metal_stdlib>
+
+using namespace metal;
+
+struct SumInput {
+    device uint *data;
+    volatile device atomic_uint *sum;
+};
+
+kernel void sum(device SumInput& input [[ buffer(0) ]],
+                uint gid [[ thread_position_in_grid ]])
+{
+    atomic_fetch_add_explicit(input.sum, input.data[gid], memory_order_relaxed);
+}";
 
 fn main() {
     autoreleasepool(|| {
